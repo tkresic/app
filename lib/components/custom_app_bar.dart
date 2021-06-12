abef0031757487f7c6ac57453a21f49a56c6692b
Toni Krešić <tonikresic1997@gmail.com>
2021-06-12 14:09:49 +0200

    CustomAppBar: [update] - remove shift information

--- lib/components/custom_app_bar.dart
+++ lib/components/custom_app_bar.dart
@@ -1,12 +1,10 @@
-import 'package:app/models/shift.dart';
+import 'dart:io';
 import 'package:app/models/user.dart';
 import 'package:app/pages/index/index.dart';
 import 'package:app/providers/user_provider.dart';
 import 'package:app/util/shared_preference.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-
 import 'middleware.dart';
 
 class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
@@ -20,40 +18,14 @@ class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
 }
 
 class _CustomAppBarState extends State<CustomAppBar> {
-  // TODO => Either always fetch last shift when changing routes or keep app bar state in between routes
-  Shift? shift;
-  Widget text = const Text('Smjena trenutno nije aktivna', style: TextStyle(fontSize: 14, color: Colors.white));
 
   @override
   Widget build(BuildContext context) {
+
     User? user = Provider.of<UserProvider>(context).user;
 
     if (user == null) {
       return const Middleware();
-    }
-
-    String _getCurrentDateTimeString() {
-      DateTime now = DateTime.now();
-      DateFormat formatter = DateFormat('dd.MM.yyyy HH:mm:ss');
-      return formatter.format(now);
-    }
-
-    void _startShift() {
-      // TODO => Start shift
-      setState(() {
-        shift = Shift(id: 1, start: _getCurrentDateTimeString(), end: null, user: user, gross: 0);
-        text = Text('Smjena započeta u ${shift!.start}', style: const TextStyle(fontSize: 14, color: Colors.white));
-      });
-    }
-
-    void _endShift() {
-      // TODO => End shift
-      setState(() {
-        shift!.end = _getCurrentDateTimeString();
-        // Shift.getData().add(shift!);
-        shift = null;
-        text = const Text('Smjena trenutno nije aktivna', style: TextStyle(fontSize: 14, color: Colors.white));
-      });
     }
 
     return AppBar(
@@ -91,21 +63,7 @@ class _CustomAppBarState extends State<CustomAppBar> {
           const Icon(Icons.person, color: Colors.white),
           const SizedBox(width: 5),
           Text(user.username, style: const TextStyle(color: Colors.white)),
-          const SizedBox(width: 30),
-          ElevatedButton(
-            onPressed: (shift == null) ? () =>  _startShift() : null,
-            child: const Text("Započni smjenu", style: TextStyle(color: Colors.white)),
-            style: ElevatedButton.styleFrom(primary: Colors.deepOrange),
-          ),
-          const SizedBox(width: 30),
-          ElevatedButton(
-            onPressed: (shift != null) ? () =>  _endShift() : null,
-            child: const Text("Završi smjenu", style: TextStyle(color: Colors.white)),
-            style: ElevatedButton.styleFrom(primary: Colors.deepOrange),
-          ),
-          const SizedBox(width: 30),
-          text,
-        ],
+        ]
       ),
       actions: <Widget>[
         Tooltip(
